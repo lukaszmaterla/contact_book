6f0158ba5da2acfdf090838c16f609a5dc86e6ef
lukasz <lukasz@student.edu>
2017-04-26 16:53:00 +0200

    create contact controller, action: show, show all, delete, update and form in views

--- src/ContactBookBundle/Controller/ContactController.php
+++ src/ContactBookBundle/Controller/ContactController.php
@@ -2,18 +2,107 @@
 
 namespace ContactBookBundle\Controller;
 
+use ContactBookBundle\Entity\Contact;
+use ContactBookBundle\Form\ContactType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends Controller
 {
     /**
      * @Route("/new")
-     * @Template("")
+     * @Template(":Contact:new.html.twig")
      */
-    public function indexAction($name)
+    public function newCreateAction(Request $request)
     {
-        return $this->render('', array('name' => $name));
+        $contact = new Contact();
+        
+        $form = $this->createForm(ContactType::class, $contact, ['action' => $this->generateUrl('contactbook_contact_newcreate')]);
+        
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            
+            $em= $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+            
+            return $this->redirectToRoute('contactbook_contact_show', ['id'=> $contact->getId()]);
+        }
+
+        return ['form' => $form->createView()];
+
+             
+    }
+    /**
+     * @Route("/{id}")
+     * @Template(":Contact:show.html.twig")
+     */
+    public function showAction($id)
+    {
+        $contact = $this->getDoctrine()->getRepository('ContactBookBundle:Contact')->find($id);
+
+        if(!$contact){
+
+            throw $this->createNotFoundException("Contact not Found");
+        }
+        return ['contact'=> $contact];
+    }
+
+    /**
+     * @Route("/")
+     * @Template(":Contact:show_all.html.twig")
+     */
+    public function showAllAction()
+    {
+        $contacts = $this->getDoctrine()->getRepository('ContactBookBundle:Contact')->findAll();
+
+        return ['contacts'=> $contacts];
+    }
+
+    /**
+     * @Route("/{id}/delete")
+     */
+
+    public function deleteAction($id)
+    {
+         $contact = $this->getDoctrine()->getRepository('ContactBookBundle:Contact')->find($id);
+
+         if(!$contact){
+             throw $this->createNotFoundException("Contact not Found");
+         }
+
+         $em = $this->getDoctrine()->getManager();
+         $em->remove($contact);
+         $em->flush();
+
+         return $this->redirectToRoute('contactbook_contact_showall');
+    }
+
+    /**
+     * @Route("/{id}/modify")
+     * @Template(":Contact:new.html.twig")
+     */
+
+    public function modifyAction(Request $request, $id)
+    {
+        $contact = $this->getDoctrine()->getRepository('ContactBookBundle:Contact')->find($id);
+        if(!$contact){
+            throw $this->createNotFoundException("Contact not Found");
+        }
+
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if($form->isValid() && $form->isSubmitted())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute('contactbook_contact_show', ['id'=> $contact->getId()]);
+        }
+
+        return ['form' => $form->createView()];
     }
 }
---
+++ src/ContactBookBundle/Form/ContactType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ContactBookBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ContactBookBundle\Entity\Contact'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'contactbookbundle_contact';
+    }
+
+
+}
